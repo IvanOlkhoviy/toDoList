68ab5c4cbb4dfc0cb4042e6a8dd71a77f443ee57
IvanOlkhoviy <ivanolkhoviy@gmail.com>
2020-09-03 09:24:16 +0300

    first commit

---
+++ src/test/java/com/olkhoviy/todolist/TodolistApplicationTests.java
@@ -0,0 +1,13 @@
+package com.olkhoviy.todolist;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TodolistApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
